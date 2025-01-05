4539a68a9c988c0a4f886e1f18b149d68f956a16
sam-zarila <zarilatroxx99@gmail.com>
2025-01-05 00:48:33 -0800

    landing page

---
+++ app/home/NewsRoom.tsx
@@ -0,0 +1,40 @@
+const news =[
+    {
+        id: 1,
+        type: "Announcement",
+        title: "Holiday Sale Announcement",
+        description:
+          "Our biggest holiday sale is starting on December 15th. Enjoy discounts of up to 50% on selected items! Don’t miss out on this fantastic opportunity to save.",
+        date: "December 10, 2024",
+        icon: "📢",
+      },
+      {
+        id: 2,
+        type: "Upcoming Event",
+        title: "Live Webinar: Market Trends 2025",
+        description:
+          "Join us for an exclusive webinar on January 5th to discuss the latest market trends and growth opportunities. Register now to secure your spot.",
+        date: "January 5, 2025",
+        icon: "📅",
+      },
+      {
+        id: 3,
+        type: "Update",
+        title: "New Products Launch",
+        description:
+          "We’re excited to announce the launch of our new eco-friendly product line. Available starting January 10th! Explore sustainable and innovative solutions.",
+        date: "January 10, 2025",
+        icon: "🆕",
+      },
+]
+
+const MarketSection =()=>{
+    return(
+        <section className="py-16 px-6">
+            <div className="mw-w-6xl mx-auto">
+                
+            </div>
+
+        </section>
+    )
+}
