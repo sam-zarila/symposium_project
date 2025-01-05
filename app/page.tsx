65922bea084cd047f04f38550c196e7274884a57
sam-zarila <zarilatroxx99@gmail.com>
2025-01-04 18:04:26 -0800

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,16 @@ import Header from "./components/header";
 import Hero from "./home/hero";
 import TrustedPartner from "./home/steps";
 import Services from "./home/services";
+import MovingLogos from "./home/MovingLogos";
+
+
 
 export default function Home(){
   return(
     <>
 <Header/> 
 <Hero/>   
+<MovingLogos/>
 {/* <Services/>  */}
 <TrustedPartner/> 
     </>
