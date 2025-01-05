7a7e2f9cf254224dae6f5ae217ef637980c42583
sam-zarila <zarilatroxx99@gmail.com>
2025-01-04 17:56:35 -0800

    landing page

---
+++ app/home/MovingLogos.ts
@@ -0,0 +1,12 @@
+import React  from 'react';
+
+
+const logos = [
+    'health.jpg', 'education.jpg', 'tourism.jpg', 'hotel.jpg', 
+    'bank.jpg', 'ict.jpg', 'construction.jpg', 'transport.jpg', 'law.jpg'
+  ];
+
+  const MovingLogos =()=>{
+
+    
+  }
