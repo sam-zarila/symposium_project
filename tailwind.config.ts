c3f2e103a2d85189b5be2555f80f22828f228de9
sam-zarila <zarilatroxx99@gmail.com>
2025-01-04 18:03:18 -0800

    landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,22 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        'move-left-to-right': 'moveLeftToRight 15s linear infinite',
+        'move-left-to-right-sm': 'moveLeftToRight 10s linear infinite', // Faster for small screens
+        'move-right-to-left': 'moveRightToLeft 15s linear infinite',
+        'move-right-to-left-sm': 'moveRightToLeft 10s linear infinite', // Faster for small screens
+      },
+      keyframes: {
+        moveLeftToRight: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+        moveRightToLeft: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
     },
   },
   plugins: [],
